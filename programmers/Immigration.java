cb7bd0778662844572cd27e160241ba473b47413
skybluenada <skybluenada@naver.com>
2021-07-22 15:04:05 +0900

    입국심사

--- programmers/Immigration.java
+++ programmers/Immigration.java
@@ -6,29 +6,26 @@ class Solution {
     public long solution(int n, int[] times) {
         long answer = 0;
         Arrays.sort(times);
-        int min = 1;
-        int max = times[times.length - 1] * n;
-        int time = max;
-        while(true) {        	
-            int cnt = 0;
-        	for(int i = 0 ; i < times.length; i++) {        		
-        		cnt += time / times[i];        		
-        		System.out.println(cnt + " ' " + times[i]);
-        	}
-        	if(cnt > n) {	// 기준보다 더 많이 검사하면, 시간을 줄여서 더 적게 검사해야함        	
-        		System.out.println("1");		
-        		max = time;
-        		time = (min + max) / 2;
-        	}
-        	if(cnt < n) {	// 기준보다 더 적게 검사하면, 시간을 늘려서 더 많이 검사해야함
-        		System.out.println("2");		
-        		min = time;
-        		time = (min + max) / 2;
-        	}
-        	if(cnt == n)
-                return time;
-        }
+        long min = 0;
+        long max = (long)times[times.length - 1] * n;
+        long mid = max;
+        while(min <= max) {        	
+        	long total = 0;
+    		mid = (min + max) / 2;
 
+        	for(int i = 0 ; i < times.length; i++) {        		
+        		total += mid / times[i];        		
+        	}
+        	if(total >= n) {	// 기준보다 더 많이 검사하면, 시간을 줄여서 더 적게 검사해야함        	
+        		max = mid - 1;
+                answer = mid;
+
+        	}
+        	if(total < n) {	// 기준보다 더 적게 검사하면, 시간을 늘려서 더 많이 검사해야함
+        		min = mid + 1;
+        	}
+        }
+        return answer;
     }
 }
 
