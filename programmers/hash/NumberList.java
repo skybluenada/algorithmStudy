3d5311d907be496015889b9ab69ad26bbb9983c5
kakaru <62199393+skybluenada@users.noreply.github.com>
2021-07-26 01:04:34 +0900

    Update NumberList.java

--- programmers/hash/NumberList.java
+++ programmers/hash/NumberList.java
@@ -2,13 +2,20 @@ package programmers.hash;
 
 import java.util.*;
 
-// 답은 맞지만,
-// 효율성 테스트 실패 내일 다시!
 public class NumberList {
 	public static void main(String[] args) {
+		String[] phoneBook1 = {"119", "97674223", "1195524421"};	// false
+		String[] phoneBook2 = {"123","456","789"};					// true
+		String[] phoneBook3 = {"12","123","1235","567","88"};		// false
 		
+		System.out.println(solution2(phoneBook1));
+		System.out.println(solution2(phoneBook2));
+		System.out.println(solution2(phoneBook3));
+		
+		System.out.println();
 	}
-    static boolean solution(String[] phone_book) {
+	// 효율성 실패
+    public static boolean solution(String[] phone_book) {
         boolean answer = true;
         Arrays.sort(phone_book);
         for(int i = 0; i < phone_book.length - 1; i++) {
@@ -20,5 +27,24 @@ public class NumberList {
         }
         return answer;
     }
+    
+    // 해쉬 이용
+    public static boolean solution2(String[] phone_book) {
+        boolean answer = true;
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < phone_book.length; i++){
+            map.put(phone_book[i],i);
+        }
+        for(int i = 0; i < phone_book.length; i++){
+            for(int j = 1; j < phone_book[i].length(); j++){
+                if(map.containsKey(phone_book[i].substring(0,j))){
+                    answer = false;
+                }
+            }
+        }
+        return answer;
+    }
+
+        
 
 }
