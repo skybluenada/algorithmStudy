c6bf9dd12f0725d5b497708c677f003643e1fb6b
kakaru <62199393+skybluenada@users.noreply.github.com>
2021-07-26 23:46:38 +0900

    Create BiggestNumber.java

---
+++ programmers/sort/BiggestNumber.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        String[] sNumbers = new String[numbers.length];
+        for(int i = 0; i < numbers.length; i++){
+            sNumbers[i] = String.valueOf(numbers[i]); 
+        }
+        Arrays.sort(sNumbers, new Comparator<String>(){
+            @Override
+            public int compare(String a, String b){
+                return (b+a).compareTo(a+b);
+            } 
+        });
+        if(sNumbers[0].equals("0")){
+            return "0";
+        }
+        for(String word : sNumbers){
+            answer += word;
+        }
+        return answer;
+    }
+}
